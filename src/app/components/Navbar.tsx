0377604b08936c5febba23eb1580983621a625b7
Pranava Thejaswi N M <96561922+PRANAVATHEJASWI@users.noreply.github.com>
2025-05-14 21:37:39 +0530

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,6 @@
 'use client';
 import Link from 'next/link';
 import '../styles/navbar.css';  // Import the CSS file
-import { useState } from 'react';
 
 export default function Navbar() {
   return (
