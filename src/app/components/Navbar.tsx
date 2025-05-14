f49430e03268d2e0ae7c642d0a5fc9cbf61bd930
Pranava Thejaswi N M <96561922+PRANAVATHEJASWI@users.noreply.github.com>
2025-05-14 22:12:24 +0530

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,20 +1,60 @@
-// components/Navbar.tsx
 'use client';
 import Link from 'next/link';
-import '../styles/navbar.css';  // Import the CSS file
-
+import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/navbar.css'; 
 export default function Navbar() {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, []);
+
   return (
-    <>
-    <nav className={`navbar`}>
-        <div className="nav-links space-x-4">
-          <Link href="/" className="hover:text-cyan-400 transition">Home</Link>
-          <Link href="#experience" className="hover:text-cyan-400 transition">Experience</Link>
-          <Link href="#projects" className="hover:text-cyan-400 transition">Projects</Link>
-          <Link href="#Certificate" className="hover:text-cyan-400 transition">Certification/Short Courses</Link>
-          <Link href="#activities" className="hover:text-cyan-400 transition">activities</Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler ms-auto"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#experience">Experience</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#projects">Projects</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#Certificate">Certifications</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                More
+              </a>
+              <ul className="dropdown-menu dropdown-menu-dark">
+                <li><Link className="dropdown-item" href="#activities">Activities</Link></li>
+                <li><a className="dropdown-item" href="#">Contact</a></li>
+              </ul>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
-    </>
   );
 }
